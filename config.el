b8738f51fb14590639c6848c1cf99b46040660e6
Tristan Floch <tristan.floch@algolia.com>
2025-09-03 15:28:48 +0200

    fix(font): rendering bug on start

--- config.el
+++ config.el
@@ -26,8 +26,8 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 
-(setq doom-font (font-spec :family "SauceCodePro Nerd Font" :size 17)
-      doom-variable-pitch-font (font-spec :family "Ubuntu Nerd Font" :size 18))
+(setq doom-font (font-spec :family "SauceCodePro Nerd Font" :weight 'regular :size 17)
+      doom-variable-pitch-font (font-spec :family "Ubuntu Nerd Font" :weight 'regular :size 18))
 
 ;; (setq catppuccin-flavor 'mocha) ;; or 'latte, 'macchiato, or 'frappe
 ;; (setq doom-theme 'doom-tokyo-night)
